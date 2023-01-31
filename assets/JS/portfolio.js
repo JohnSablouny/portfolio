5971559c08d83fbbcf8e88eddd3a837df0a23dd9
JohnSablouny <114349287+JohnSablouny@users.noreply.github.com>
2023-01-31 14:57:45 +0200

    Add files via upload

---
+++ assets/JS/portfolio.js
@@ -0,0 +1,110 @@
+/*=============== SHOW SIDEBAR ===============*/
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+
+/*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContent = document.querySelectorAll("[data-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContent.forEach((tabContent) => {
+      tabContent.classList.remove("skills__active");
+    });
+    target.classList.add("skills__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("skills__active");
+    });
+    tab.classList.add("skills__active");
+  });
+});
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+/*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll(".work__item");
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
+/*===== Work Popup =====*/
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("work__button")) {
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+function togglePortfolioPopup() {
+  document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+document
+  .querySelector(".portfolio__popup")
+  .addEventListener("click", togglePortfolioPopup);
+function portfolioItemDetails(portfolio) {
+  document.querySelector(".pp__thumbnail img").src =
+    portfolio.querySelector(".work__img").src;
+  document.querySelector(".portfolio__popup-subtitle span").innerHTML =
+    portfolio.querySelector(".work__title").innerHTML;
+  document.querySelector(".portfolio__popup-body").innerHTML =
+    portfolio.querySelector(".portfolio__item-details").innerHTML;
+}
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modelBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+modelBtns.forEach((modelBtn, i) => {
+  modelBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
+/*=============== SWIPER TESTIMONIAL ===============*/
+let swiper = new Swiper(".testimonials__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+/*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll(".input");
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
